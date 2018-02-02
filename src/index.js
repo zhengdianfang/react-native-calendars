eff2502064d1b5ff16c34bcad9eca504bd634e4f
qlyan(benben) <qlyan@thoughtworks.com>
2018-02-02 14:20:25 +0800

    export dateutls and Day component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export { default as Calendar } from './calendar';
 export { default as CalendarList } from './calendar-list';
 export { default as Agenda } from './agenda';
+export { default as dateutils } from './dateutils';
+export { default as Day } from './calendar/day/basic';
 export { default as LocaleConfig } from 'xdate';
